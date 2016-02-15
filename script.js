9dd7a49d93587e77b224b8cdead52e59f83da0da
jason goldfarb <jasongoldfarb@jasons-MacBook-Pro.local>
2016-02-15 14:04:32 -0700

    Lights work

--- script.js
+++ script.js
@@ -11,27 +11,21 @@
 
 var button = document.getElementById("stopButton");
 button.addEventListener('click', function () {
-document.querySelector('stopLight').style.backgroundColor = 'red';
-};
+document.getElementById('stopLight').style.backgroundColor = 'red';
+});
 
 
 var button = document.getElementById("slowButton");
 button.addEventListener('click', function () {
-document.querySelector('slowLight').style.backgroundColor = 'red';
-};
+document.getElementById('slowLight').style.backgroundColor = 'yellow';
+});
 
 
 var button = document.getElementById("goButton");
 button.addEventListener('click', function () {
-document.querySelector('goLight').style.backgroundColor = 'red';
-};
+document.getElementById('goLight').style.backgroundColor = 'green';
+});
 //
 // // var stopButton = document.getElementById("stopButton");
 //   if (stopButton.firstChild.nodeValue = "stopButton") {
 //     document.getElementById("stoplight").style.backgroundColor = "red";
-
-
-
-  }
-  }
-}
